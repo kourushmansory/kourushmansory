8de062e1172ef39fa751ab116ca27593adc26a60
kourushmansory <olaebal@gmail.com>
2024-03-29 11:55:08 +0300

    Изменение 1

--- zadanie 4.py
+++ zadanie 4.py
@@ -1,33 +1,34 @@
-from random import  *
+from random import *
+
+
 def login(x):
     """
     Функция для создания догина пользователя
     x - все данные о пользователе
     return - логин вида Фамилия_ИО
     """
-    f,i,o = x[1].split()
+    f, i, o = x[1].split()
     ln = f'{f}_{i[0]}{o[0]}'
     return ln
 
-def password():
 
-    A='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    a= A.lower()
-    c='0123456789'
-    p = [choice(A),choice(A),choice(A),choice(a),choice(a),choice(a),choice(c),choice(c)]
+def password():
+    A = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    a = A.lower()
+    c = '0123456789'
+    p = [choice(A), choice(A), choice(A), choice(a), choice(a), choice(a), choice(c), choice(c)]
     shuffle(p)
     p = ''.join(p)
     return p
+# ПУМПУРУМ
 
-
-
-a=open('students.csv', encoding='utf8').readlines()
+a = open('students.csv', encoding='utf8').readlines()
 shapka = a.pop(0)
 for i in range(len(a)):
-    a[i] =a[i].strip().split(',')
+    a[i] = a[i].strip().split(',')
     a[i] += [login(a[i]), password()]
 
 f = open('students_password.csv', 'w', encoding='utf8')
 f.write(shapka.strip() + ',login,password\n')
 for x in a:
-    f.write(','.join(x)+'\n')
+    f.write(','.join(x) + '\n')
